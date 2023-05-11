e8824499eead4472e3124f09c2e134c000f2bc99
Ealvarez01 <e.alvarez2404@yahoo.com>
2023-05-10 23:04:23 -0700

    working on css styling

--- preparedness-frontend/src/App.js
+++ preparedness-frontend/src/App.js
@@ -1,21 +1,88 @@
-import React from "react"
-import { Button, Input, Label } from "reactstrap"
+import React, {useState} from "react"
+import { Button, Input, Label, FormGroup, Form } from "reactstrap"
 import ModalComponent from "./components/ModalComponent"
 import "./App.css"
+import background from './assets/background-image.jpeg'
+
 
 const App = () => {
+
+  //creates a function to set the user input by passing props in useState 
+  const [input, setInput] = useState(" ")
+
+  //creates a function when user clicks on button to handle the changing of the click by listening and targeting the value of whatever the change is.
+  const handleChange = (e) => {
+    setInput(e.target.value)
+  }
+
+  //creates a modal, sets the modal and assigns it to useState to pass props to say if whatever is passed in the modal input if it is false or not.
+  const [modal, setModal] = useState(false)
+
+  //creates a function to allow the toggle to connect to the modal if it is not true. 
+  const toggle = () => setModal(!modal)
+
+  //creates a function to reset the user name when the reset button is clicked. 
+  const handleReset = (e) => {
+    setInput(" ")
+  }
+
+  const myStyle={
+    backgroundImage: `url(${background}`,
+    height:'100vh',
+    marginTop:'0px',
+    backgroundSize: 'cover',
+    backgroundRepeat: 'no-repeat',
+    
+};
+
   return (
-    <div className="entire-content">
-      <h1>Preparedness Assessment</h1>
+    
+    <div 
+      className="entire-content"
+      style={myStyle}>
+
+      <h1 className="header">Preparedness Assessment</h1>
+
+    <br/>
+      <Form>
       <div className="form">
         <div className="input">
           <Label for="name">Enter your name</Label>
-          <Input />
+        <FormGroup>
+          <Input 
+            style={{
+              marginBottom: '20px',
+              display: 'flex',
+              flexDirection: 'column'}}
+              onChange={handleChange} />
+        </FormGroup>
         </div>
-        <Button>Click Me</Button>
-        <Button>Reset</Button>
-        <ModalComponent />
+        <FormGroup>
+        <Button 
+          style={{
+            color: 'white',
+            backgroundColor: '#0dcaf0',
+            paddingBlock: '10px 10px 10px 10px',
+            margin: '20px',
+            marginTop: '20px'
+          }}
+          onClick={toggle}>Click Me</Button>
+        </FormGroup>
+        <FormGroup>
+        <Button 
+          style={{
+            color: 'white',
+            backgroundColor: '#0dcaf0',
+            paddingBlock: '10px 10px 10px 10px',
+            margin: '20px',
+            marginTop: '20px'
+          }}
+          className="clickMe" 
+          onClick={handleReset}>Reset</Button>
+        </FormGroup>
+        <ModalComponent toggle={toggle} input={input} modal={modal}/>
       </div>
+      </Form>
     </div>
   )
 }
--- preparedness-frontend/src/components/ModalComponent.js
+++ preparedness-frontend/src/components/ModalComponent.js
@@ -1,7 +1,19 @@
 import React from "react"
+import {Modal, ModalHeader, ModalBody} from 'reactstrap'
 
-const ModalComponent = () => {
-  return <></>
+
+const ModalComponent = ({input, toggle, modal}) => {
+  return(
+    <div>
+      <Modal isOpen={modal} toggle={toggle}>
+        <ModalHeader toggle={toggle}>Hello There {input}!
+        </ModalHeader>
+        <ModalBody>
+          Find empty spot in cupboard and sleep all day nap all day, for leave hair on owner's clothes and jump on fridge or meow hide head under blanket so no one can see go crazy with excitement when plates are clanked together signalling the arrival of cat food. Disappear for four days and return home with an expensive injury; bite the vet bleghbleghvomit my furball really tie the room together yet oooo! dangly balls! jump swat swing flies so sweetly to the floor crash move on wash belly nap purr like an angel, but run up and down stairs so wake up human for food at 4am. Dismember a mouse and then regurgitate parts of it on the family room floor lick butt and make a weird face but rub against owner because nose is wet more napping, more napping all the napping is exhausting stand in doorway, unwilling to chose whether to stay in or go out and pee in the shoe. Pooping rainbow while flying in a toasted bread costume in space.
+        </ModalBody>
+      </Modal>
+    </div>
+  )
 }
 
 export default ModalComponent
